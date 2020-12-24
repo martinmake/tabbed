0a41d612a29462f6fe57803d2caa138aec69ab02
Martin Michalec <martin.michalec.2003@gmail.com>
2020-12-24 13:22:45 +0100

    common

--- config.def.h
+++ config.def.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "monospace:size=9";
-static const char* normbgcolor  = "#222222";
-static const char* normfgcolor  = "#cccccc";
-static const char* selbgcolor   = "#555555";
-static const char* selfgcolor   = "#ffffff";
-static const char* urgbgcolor   = "#111111";
-static const char* urgfgcolor   = "#cc0000";
+static const char font[]        = "xos4 Terminus:pixelsize=10";
+static const char* normbgcolor  = "#000000";
+static const char* normfgcolor  = "#ffffff";
+static const char* selbgcolor   = "#ffff00";
+static const char* selfgcolor   = "#000000";
+static const char* urgbgcolor   = "#ff0000";
+static const char* urgfgcolor   = "#000000";
 static const char before[]      = "<";
 static const char after[]       = ">";
 static const char titletrim[]   = "...";
@@ -20,8 +20,12 @@ static       Bool urgentswitch  = False;
  * then the current position is changed + newposition. If npisrelative
  * is False, then newposition is an absolute position.
  */
-static int  newposition   = 0;
-static Bool npisrelative  = False;
+// always after
+static int  newposition   = 1;
+static Bool npisrelative  = True;
+// always first
+// static int  newposition   = 0;
+// static Bool npisrelative  = False;
 
 #define SETPROP(p) { \
         .v = (char *[]){ "/bin/sh", "-c", \
@@ -33,34 +37,52 @@ static Bool npisrelative  = False;
         } \
 }
 
-#define MODKEY ControlMask
+#define AltMask Mod1Mask
 static Key keys[] = {
-	/* modifier             key        function     argument */
-	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
+	/* modifier             key           functin      argument                       */
+	{          ControlMask ,XK_Return    ,focusonce   ,{ 0 }                          },
+	{          ControlMask ,XK_Return    ,spawn       ,{ 0 }                          },
+	{  AltMask|ControlMask ,XK_Return    ,focusonce   ,{ 0 }                          },
+	{  AltMask|ControlMask ,XK_Return    ,spawn       ,{ 0 }                          },
 
-	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
-	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
+	{  AltMask|ControlMask ,XK_l         ,rotate      ,{ .i = +1 }                    },
+	{  AltMask|ControlMask ,XK_h         ,rotate      ,{ .i = -1 }                    },
+	{  AltMask|ControlMask ,XK_j         ,movetab     ,{ .i = -1 }                    },
+	{  AltMask|ControlMask ,XK_k         ,movetab     ,{ .i = +1 }                    },
+	{          ControlMask ,XK_Tab       ,rotate      ,{ .i =  0 }                    },
 
-	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
-	{ MODKEY,               XK_1,      move,        { .i = 0 } },
-	{ MODKEY,               XK_2,      move,        { .i = 1 } },
-	{ MODKEY,               XK_3,      move,        { .i = 2 } },
-	{ MODKEY,               XK_4,      move,        { .i = 3 } },
-	{ MODKEY,               XK_5,      move,        { .i = 4 } },
-	{ MODKEY,               XK_6,      move,        { .i = 5 } },
-	{ MODKEY,               XK_7,      move,        { .i = 6 } },
-	{ MODKEY,               XK_8,      move,        { .i = 7 } },
-	{ MODKEY,               XK_9,      move,        { .i = 8 } },
-	{ MODKEY,               XK_0,      move,        { .i = 9 } },
+	{          ControlMask ,XK_s         ,spawn,       SETPROP("_TABBED_SELECT_TAB")  },
 
-	{ MODKEY,               XK_q,      killclient,  { 0 } },
+	{          ControlMask ,XK_1         ,move        ,{ .i = 0 }                     },
+	{          ControlMask ,XK_2         ,move        ,{ .i = 1 }                     },
+	{          ControlMask ,XK_3         ,move        ,{ .i = 2 }                     },
+	{          ControlMask ,XK_4         ,move        ,{ .i = 3 }                     },
+	{          ControlMask ,XK_5         ,move        ,{ .i = 4 }                     },
+	{          ControlMask ,XK_6         ,move        ,{ .i = 5 }                     },
+	{          ControlMask ,XK_7         ,move        ,{ .i = 6 }                     },
+	{          ControlMask ,XK_8         ,move        ,{ .i = 7 }                     },
+	{          ControlMask ,XK_9         ,move        ,{ .i = 8 }                     },
+	{          ControlMask ,XK_0         ,move        ,{ .i = 9 }                     },
+	{  AltMask|ControlMask ,XK_1         ,move        ,{ .i = 0 }                     },
+	{  AltMask|ControlMask ,XK_2         ,move        ,{ .i = 1 }                     },
+	{  AltMask|ControlMask ,XK_3         ,move        ,{ .i = 2 }                     },
+	{  AltMask|ControlMask ,XK_4         ,move        ,{ .i = 3 }                     },
+	{  AltMask|ControlMask ,XK_5         ,move        ,{ .i = 4 }                     },
+	{  AltMask|ControlMask ,XK_6         ,move        ,{ .i = 5 }                     },
+	{  AltMask|ControlMask ,XK_7         ,move        ,{ .i = 6 }                     },
+	{  AltMask|ControlMask ,XK_8         ,move        ,{ .i = 7 }                     },
+	{  AltMask|ControlMask ,XK_9         ,move        ,{ .i = 8 }                     },
+	{  AltMask|ControlMask ,XK_0         ,move        ,{ .i = 9 }                     },
 
-	{ MODKEY,               XK_u,      focusurgent, { 0 } },
-	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
+	{          ControlMask ,XK_q         ,killclient  ,{ 0 }                          },
 
-	{ 0,                    XK_F11,    fullscreen,  { 0 } },
+	{  AltMask|ControlMask ,XK_u         ,focusurgent ,{ 0 }                          },
+	{          ControlMask ,XK_u         ,toggle      ,{ .v = (void*) &urgentswitch } },
+
+	{  0                   ,XK_F11       ,fullscreen  ,{ 0 }                          },
+
+	{          ControlMask ,XK_Alt_L     ,showbar     ,{ .i = 1 }                     },
+	{  AltMask             ,XK_Control_L ,showbar     ,{ .i = 1 }                     }, }; static Key keyreleases[] = {
+	{  AltMask|ControlMask ,XK_Alt_L     ,showbar     ,{ .i = 0 }                     },
+	{  AltMask|ControlMask ,XK_Control_L ,showbar     ,{ .i = 0 }                     },
 };
